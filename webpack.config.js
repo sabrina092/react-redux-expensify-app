bc061a7c015123a0e79801779294dbcaa3faeea8
sabrina092 <sabrinasobhan.92@gmail.com>
2019-09-29 23:22:47 -0400

    Setup devdependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,44 +8,46 @@ module.exports = (env) => {
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public','dist'),
       filename: "package.js"
-  },
-  module: {
-     rules:[{
-       loader:'babel-loader',
-       test:/\.js$/,
-       exclude:/node_modules/
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
       }, {
 
-       test:/\.s?css$/,
-       use: CSSExtract.extract({
-         use:[
-           {
-             loader:'css-loader',
-             options : {
-               sourceMap:true
-             }
-           },
-           {
-            loader:'sass-loader',
-            options : {
-              sourceMap:true
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
             }
-           }
-         ]
-       })
-     }]  
-    
-  },
+          ]
+        })
+      }]
 
-  plugins:[
-    CSSExtract
-  ],
-  devtool:isProduction ? 'source-map':'inline-source-map',
-  devServer:{
-    contentBase:path.join(__dirname, 'public'),
-    historyApiFallback:true
-   }
- };
+    },
+
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath:'/dist/'
+    
+    }
+  };
 };
